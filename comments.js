0d8bf87baa1fca68d268b55dd4606647a48e6886
xiinthanh <118944173+xiinthanh@users.noreply.github.com>
2024-04-16 08:59:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,59 +1,30 @@
-// Create web server
-// Run server
-// Create a new comment
-// Get all comments
-// Get a single comment
-// Update a comment
-// Delete a comment
-// Run server
-// Test with Postman
-
-// Create web server
+// Create web server and listen on port 3000
 const express = require('express');
 const app = express();
-app.use(express.json());
-
-// Run server
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
-// Create a new comment
-const comments = [];
-app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comments.push(comment);
-  res.send('Comment is added to the database');
-});
-
-// Get all comments
+// Set up a route to listen on http://localhost:3000/comments
 app.get('/comments', (req, res) => {
-  res.send(comments);
+    res.send('Comments route');
 });
 
-// Get a single comment
-app.get('/comments/:id', (req, res) => {
-  const id = req.params.id;
-  const comment = comments[id];
-  res.send(comment);
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
 
-// Update a comment
-app.put('/comments/:id', (req, res) => {
-  const id = req.params.id;
-  const newComment = req.body;
-  comments[id] = newComment;
-  res.send('Comment has been updated');
+// Path: index.js
+// Create web server and listen on port 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set up a route to listen on http://localhost:3000
+app.get('/', (req, res) => {
+    res.send('Home route');
 });
 
-// Delete a comment
-app.delete('/comments/:id', (req, res) => {
-  const id = req.params.id;
-  comments.splice(id, 1);
-  res.send('Comment has been deleted');
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
-
-// Run server
-// Test with Postman
-// Create a
